fa966eb6c50c97e6e25777b7ca6458a17c0f0822
Jack Consoli <jack@consoli-solutions.com>
2025-10-20 06:13:52 -0400

    Add files via upload

--- brcdapi_rest.py
+++ brcdapi_rest.py
@@ -9,7 +9,7 @@ Unless required by applicable law or agreed to in writing, software distributed 
 language governing permissions and limitations under the License.
 
 The license is free for single customer use (internal applications). Use of this module in the production,
-redistribution, or service delivery for commerce requires an additional license. Contact jack@consoli-solutions.com for
+redistribution, or service delivery for commerce requires an additional license. Contact jack_consoli@yahoo.com for
 details.
 
 **Description**
@@ -68,15 +68,17 @@ such as the brcddb libraries, to control what gets printed to the log.
 | 4.0.4     | 25 Aug 2025   | FOS doesn't return OPTIONS in the OPTIONS request, so it was artificially added if a  |
 |           |               | valid response (no errors) was returned for a URI with an OPTIONS request.            |
 +-----------+---------------+---------------------------------------------------------------------------------------+
+| 4.0.5     | 19 Oct 2025   | Updated comments only.                                                                |
++-----------+---------------+---------------------------------------------------------------------------------------+
 """
 __author__ = 'Jack Consoli'
-__copyright__ = 'Copyright 2023, 2024, 2025 Consoli Solutions, LLC'
-__date__ = '25 Aug 2025'
+__copyright__ = 'Copyright 2024, 2025 Consoli Solutions, LLC'
+__date__ = '19 Oct 2025'
 __license__ = 'Apache License, Version 2.0'
 __email__ = 'jack_consoli@yahoo.com'
 __maintainer__ = 'Jack Consoli'
 __status__ = 'Released'
-__version__ = '4.0.4'
+__version__ = '4.0.5'
 
 import http.client
 import re
--- fos_cli.py
+++ fos_cli.py
@@ -9,7 +9,7 @@ Unless required by applicable law or agreed to in writing, software distributed 
 language governing permissions and limitations under the License.
 
 The license is free for single customer use (internal applications). Use of this module in the production,
-redistribution, or service delivery for commerce requires an additional license. Contact jack@consoli-solutions.com for
+redistribution, or service delivery for commerce requires an additional license. Contact jack_consoli@yahoo.com for
 details.
 
 **Description**
@@ -54,15 +54,17 @@ do anything with prompts and doesn't perform any error checking.
 +-----------+---------------+---------------------------------------------------------------------------------------+
 | 4.0.3     | 25 Aug 2025   | Added default_cli_wait_time()                                                         |
 +-----------+---------------+---------------------------------------------------------------------------------------+
+| 4.0.4     | 19 Oct 2025   | Updated comments only.                                                                |
++-----------+---------------+---------------------------------------------------------------------------------------+
 """
 __author__ = 'Jack Consoli'
-__copyright__ = 'Copyright 2023, 2024, 2025 Consoli Solutions, LLC'
-__date__ = '25 Aug 2025'
+__copyright__ = 'Copyright 2024, 2025 Consoli Solutions, LLC'
+__date__ = '19 Oct 2025'
 __license__ = 'Apache License, Version 2.0'
-__email__ = 'jack@consoli-solutions.com, jack_consoli@yahoo.com'
+__email__ = 'jack_consoli@yahoo.com'
 __maintainer__ = 'Jack Consoli'
 __status__ = 'Released'
-__version__ = '4.0.3'
+__version__ = '4.0.4'
 
 import time
 import paramiko
